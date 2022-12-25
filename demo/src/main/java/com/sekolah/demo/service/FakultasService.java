45292dedbc929cbe492732f1863715dc2456bf17
Yuniar Kurniawan Suroso <35044741+yuniarkurniawan@users.noreply.github.com>
2022-12-25 14:09:32 +0700

    Add files via upload

--- demo/src/main/java/com/sekolah/demo/service/FakultasService.java
+++ demo/src/main/java/com/sekolah/demo/service/FakultasService.java
@@ -1,6 +1,7 @@
 package com.sekolah.demo.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 import com.sekolah.demo.entity.Fakultas;
 
 public interface FakultasService {
+	public Map<String, Object> getAllFakultasCriteriaMap(int page, int size, String search);
+	public List<Fakultas> getAllFakultasCriteria(int page, int size, String search);
 	public List<Fakultas> getAllFakultas();
 	public Page<Fakultas> getAllFakultasPageable(Pageable pageable);
 	public Page<Fakultas> getAllFakultasFilterPageable(String keterangan, String nama, Pageable pageable);
