69494a5eb8494dc57c5a0214a06a2b6063472128
Yuniar Kurniawan Suroso <35044741+yuniarkurniawan@users.noreply.github.com>
2022-12-25 14:10:06 +0700

    Add files via upload

--- demo/src/main/java/com/sekolah/demo/service/impl/FakultasServiceImpl.java
+++ demo/src/main/java/com/sekolah/demo/service/impl/FakultasServiceImpl.java
@@ -1,6 +1,8 @@
 package com.sekolah.demo.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,11 +13,86 @@ import com.sekolah.demo.entity.Fakultas;
 import com.sekolah.demo.repository.FakultasRepository;
 import com.sekolah.demo.service.FakultasService;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
 @Service
 public class FakultasServiceImpl implements FakultasService {
 
 	@Autowired
 	private FakultasRepository fakultasRepository;
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	@Override
+	public List<Fakultas> getAllFakultasCriteria(int page, int size, String search){
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Fakultas> query = builder.createQuery(Fakultas.class);
+		Root<Fakultas> root = query.from(Fakultas.class);
+		
+		Expression<String> pathNamaFakultas = root.get("namaFakultas");
+		Expression<String> lowerNamaFakultas = builder.lower(pathNamaFakultas);
+		
+		Expression<String> pathKeteranganFakultas = root.get("keteranganFakultas");
+		Expression<String> lowerKeteranganFakultas = builder.lower(pathKeteranganFakultas);
+		
+		Predicate predicateNamaFakultas = builder.like(lowerNamaFakultas, "%"+search+"%");
+		Predicate predicateKeteranganFakultas = builder.like(lowerKeteranganFakultas, "%"+search+"%");
+		
+		query.where(builder.or(predicateNamaFakultas,predicateKeteranganFakultas));
+		query.orderBy(builder.asc(root.get("namaFakultas")));
+		
+		List<Fakultas> result = entityManager
+				.createQuery(query.select(root))
+				.setMaxResults(size)
+				.setFirstResult(page)
+				.getResultList();
+		
+		return result;
+	}
+	
+	
+	@Override
+	public Map<String, Object> getAllFakultasCriteriaMap(int page, int size, String search){
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Fakultas> query = builder.createQuery(Fakultas.class);
+		Root<Fakultas> root = query.from(Fakultas.class);
+		
+		Expression<String> pathNamaFakultas = root.get("namaFakultas");
+		Expression<String> lowerNamaFakultas = builder.lower(pathNamaFakultas);
+		
+		Expression<String> pathKeteranganFakultas = root.get("keteranganFakultas");
+		Expression<String> lowerKeteranganFakultas = builder.lower(pathKeteranganFakultas);
+		
+		Predicate predicateNamaFakultas = builder.like(lowerNamaFakultas, "%"+search+"%");
+		Predicate predicateKeteranganFakultas = builder.like(lowerKeteranganFakultas, "%"+search+"%");
+		
+		query.where(builder.or(predicateNamaFakultas,predicateKeteranganFakultas));
+		query.orderBy(builder.asc(root.get("namaFakultas")));
+		
+		List<Fakultas> result = entityManager
+				.createQuery(query.select(root))
+				.setMaxResults(size)
+				.setFirstResult(page)
+				.getResultList();
+		
+		List<Fakultas> total = entityManager
+				.createQuery(query.select(root))
+				.getResultList();
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("data", result);
+		data.put("current_page", page);
+		data.put("total_items", total.size());
+		data.put("total_pages", (total.size())/size);
+				
+		return data;
+	}
 	
 	@Override
 	public Page<Fakultas> getAllFakultasPageable(Pageable pageable){
